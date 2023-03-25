b2c01fd6a5259798115c5e67a573ed1b877d27a6
ahsun rahim <ahsunrahim@gmail.com>
2023-03-26 01:28:11 +0600

    final commit

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ const port = process.env.PORT || 5000
 const app = express()
 
 // middleware
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  next()
-})
 app.use(cors())
 app.use(express.json())
 
