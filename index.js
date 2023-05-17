45ea2e07044d3fef87323069d83c3187059c35ba
ahsun rahim <ahsunrahim@gmail.com>
2023-05-18 03:19:02 +0600

    try to add picture by multer

--- index.js
+++ index.js
@@ -1,116 +1,176 @@
-const express = require('express')
-const cors = require('cors')
-const jwt = require('jsonwebtoken')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const res = require('express/lib/response')
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const res = require("express/lib/response");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-const app = express()
+//file upload folder
+const UPLOADS_FOLDER = "./uploads/";
+
+// define the storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, UPLOADS_FOLDER);
+  },
+  filename: (req, file, cb) => {
+    const fileExt = path.extname(file.originalname);
+    const fileName =
+      file.originalname
+        .replace(fileExt, "")
+        .toLowerCase()
+        .split(" ")
+        .join("-") +
+      "-" +
+      Date.now();
+
+    cb(null, fileName + fileExt);
+  },
+});
+
+// multer upload object
+let upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1000000, // 1MB
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.fieldname === "productPicture") {
+      if (
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/jpeg"
+      ) {
+        cb(null, true);
+      }
+    } else {
+      cb(new Error("There was an unknown error!"));
+    }
+  },
+});
+
+// express app initialization
+const app = express();
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //jwt verify
 function verifyJWT(req, res, next) {
-  const authHeader = req.headers.authorization
+  const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).send({ message: 'unauthorized access' })
+    return res.status(401).send({ message: "unauthorized access" });
   }
-  const token = authHeader.split(' ')[1]
+  const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ message: 'forbidden access' })
+      return res.status(401).send({ message: "forbidden access" });
     }
-    console.log('decoded', decoded)
-    req.decoded = decoded
-    next()
-  })
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
 }
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kd4xv.mongodb.net/?retryWrites=true&w=majority`
+// define database MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kd4xv.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
-})
+});
 
 async function run() {
   try {
-    await client.connect()
-    const serviceCollection = client.db('BoguraAcCorner').collection('service')
-    const orderCollection = client.db('BoguraAcCorner').collection('order')
+    await client.connect();
+    const productCollection = client.db("BoguraAcCorner").collection("product");
+    const orderCollection = client.db("BoguraAcCorner").collection("order");
 
     //Auth
-    app.post('/login', async (req, res) => {
-      const user = req.body
+    app.post("/login", async (req, res) => {
+      const user = req.body;
       const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '1d',
-      })
-      res.send({ accessToken })
-    })
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
 
-    // service api
-    app.get('/service', async (req, res) => {
-      const query = {}
-      const cursor = serviceCollection.find(query)
-      const services = await cursor.toArray()
-      res.send(services)
-    })
+    // product api
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
 
-    app.get('/service/:id', async (req, res) => {
-      const id = req.params.id
-      const query = { _id: ObjectId(id) }
-      const service = await serviceCollection.findOne(query)
-      res.send(service)
-    })
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
 
     //post
-    app.post('/service', async (req, res) => {
-      const newService = req.body
-      const result = await serviceCollection.insertOne(newService)
-      res.send(result)
-    })
+    app.post("/service", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
 
     //delete
-    app.delete('/service/:id', async (req, rse) => {
-      const id = req.params.id
-      const query = { _id: ObjectId(id) }
-      const result = await serviceCollection.deleteOne(query)
-      res.send(result)
-    })
+    app.delete("/service/:id", async (req, rse) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // order collection api
-    app.get('/orders', verifyJWT, async (req, res) => {
-      const decodedEmail = req.decoded.email
-      const email = req.query.email
+    app.get("/orders", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+      const email = req.query.email;
       if (email === decodedEmail) {
-        const query = { email: email }
-        const cursor = orderCollection.find(query)
-        const orders = await cursor.toArray()
-        res.send(orders)
+        const query = { email: email };
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
       } else {
-        res.status(401).send({ messag: 'forbidden access' })
+        res.status(401).send({ message: "forbidden access" });
       }
-    })
+    });
 
-    app.post('/orders', async (req, res) => {
-      const orders = req.body
-      const result = await orderCollection.insertOne(orders)
-      res.send(result)
-    })
-  } finally {
-  }
+    app.post("/orders", async (req, res) => {
+      const orders = req.body;
+      const result = await orderCollection.insertOne(orders);
+      res.send(result);
+    });
+  } catch (error) {}
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
 
-//api
-app.get('/', (req, res) => {
-  res.send('Running car service')
-})
+//application route
+app.get("/", upload.single("productPicture"), (req, res, next) => {
+  res.send("Running car service Successfully");
+});
+
+// default error handler
+app.use((err, req, res, next) => {
+  if (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(500).send("There was an upload error!");
+    } else {
+      res.status(500).send(err.message);
+    }
+  } else {
+    res.send("Running car service Successfully");
+  }
+});
 
 app.listen(port, () => {
-  console.log('Listening to port', port)
-})
+  console.log("Listening to port", port);
+});
